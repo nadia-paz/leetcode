4c66b2168763b0dc201435c907f4e4421cf4cbbb
Nadia Paz <nadina.abouhamdan@gmail.com>
2024-05-30 13:50:39 -0500

    rank score pandas

---
+++ database/178.RankScore.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
+    scores = scores.assign(
+        rank = scores.score.rank(method="dense", ascending=False)
+    )
+    return scores[['score', 'rank']].sort_values('rank')
+    
