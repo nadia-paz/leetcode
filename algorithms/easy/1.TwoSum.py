ae2b522db56d7ce9c3eba8889799d7d8c809a295
Nadia Paz <nadina.abouhamdan@gmail.com>
2024-07-23 17:30:37 -0500

    add O(N)

--- algorithms/easy/1.TwoSum.py
+++ algorithms/easy/1.TwoSum.py
@@ -7,26 +7,59 @@ class Solution:
     """
 
     def twoSum1(self, nums: list[int], target: int) -> list[int]:
-        # with prints
-        for i in range(len(nums) - 1):
-            print(nums[i])
-            for j in range(i+1, len(nums)):
-                print(nums[j])
-                print("sum of nums[i] + nums[j] =", nums[i] + nums[j])
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-                    break
-
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # brute force solution O(N^2)
         for i in range(len(nums) - 1):
             
-            for j in range(i + 1, len(nums)):
+            for j in range(i+1, len(nums)):
                 
                 if nums[i] + nums[j] == target:
                     return [i, j]
-                    break
+                    # break
+def two_sum(nums, target):
+    # O(N) solution
+    hm = {}
+    for i, n in enumerate(nums):
+        
+        diff = target - n
+        
+        if diff in hm:
+            
+            return [hm[diff], i]
+        hm[n] = i
+        print
+    return []
+
+def two_sum1(nums: list, target: int):
+    # O(N) 
+    hm = {}
+    current_number, second_index = -100, -100
+    for i in range(len(nums)):
+        # print('i is', i, 'nums[i] is', nums[i])
+        current_number = nums[i]
+        diff = target - current_number
+        # print('diff is', diff)
+        if current_number in hm.keys():
+            second_index = i
+            break
+        else:
+            hm[diff] = i
+    if current_number == -100 or second_index == -100:
+        return []
+    else:
+        return [hm[current_number], second_index]
+
+
 
 if __name__=="__main__":
-    a = Solution()
+    # a = Solution()
     # print(a.twoSum([7, 8, 2, 11, 3, 4, 5], 6))
-    print(a.twoSum([3, 2, 4], 6))
+    # print(a.twoSum1([3, 2, 4], 6))
+
+    print(two_sum([5, 1, 7, 2, 9, 3], 10))  
+    print(two_sum([4, 2, 11, 7, 6, 3], 9))  
+    print(two_sum([10, 15, 5, 2, 8, 1, 7], 12))  
+    print(two_sum([1, 3, 5, 7, 9], 10))  
+    print ( two_sum([1, 2, 3, 4, 5], 10) )
+    print ( two_sum([1, 2, 3, 4, 5], 7) )
+    print ( two_sum([1, 2, 3, 4, 5], 3) )
+    print ( two_sum([], 0) )
