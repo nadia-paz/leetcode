cef76904c0bc87e1c250eaa70bc3c25c95536cbd
Nadia Paz <nadina.abouhamdan@gmail.com>
2024-10-02 08:45:25 -0500

    swap nodes in binary tree

---
+++ algorithms/easy/invertTree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root is None:
+            return None
+        # left = root.right
+        right = root.left
+        root.left = self.invertTree(root.right)
+        root.right = self.invertTree(right)
+        return root
